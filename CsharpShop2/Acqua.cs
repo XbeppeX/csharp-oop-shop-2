ddc9d156115809afd04c4373640d5e5886498ec9
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-12 16:23:49 +0200

    AcquaDone

--- CsharpShop2/Acqua.cs
+++ CsharpShop2/Acqua.cs
@@ -4,30 +4,107 @@ namespace CsharpShop2
 	public class Acqua : Prodotto
 	{
 		private double litri;
+		private readonly double ph;
+		private string sorgente;
+		public static readonly double massimaDimensioneBottiglia = 1.5;
+		public static readonly double gallone = 3.785;
 
-		public Acqua(string nome, string descrizione, double prezzo, int iva, double litri) : base(nome, descrizione, prezzo, iva)
+
+		public Acqua(string nome, string descrizione, double prezzo, int iva, double litri, double ph, string sorgente) : base(nome, descrizione, prezzo, iva)
 		{
 			this.litri = litri;
-			DimensioneBottiglia();
+			this.ph = ph;
+			this.sorgente = sorgente;
+			CalcoloDimensioneBottiglia();
 		}
 
-		public void DimensioneBottiglia()
+		public string GetSorgente()
+		{
+			return sorgente;
+		}
+
+		public double GetPh()
+		{
+			return ph;
+		}
+
+		public double GetLitri()
+		{
+			return litri;
+		}
+
+		public void Svuota()
         {
-			if (litri <= 1.5)
+			if (this.litri > 0 && this.litri <= massimaDimensioneBottiglia)
+			{				
+				Console.WriteLine("Hai svuotato la tua bottiglia di acqua" );
+				this.litri = 0;
+			}
+			else
+			{
+				Console.WriteLine("Bottiglia già vuota, non puoi svuotare ancora");
+			}
+		}
+
+		public void Riempi(double litriDaRiempire)
+		{
+			if (this.litri + litriDaRiempire <= massimaDimensioneBottiglia)
+			{
+				this.litri = this.litri + litriDaRiempire;
+				Console.WriteLine("Hai riempito la tua bottiglia di" + litriDaRiempire);
+			}
+			else
+			{
+				Console.WriteLine("Bottiglia piena, non puoi riempire di più.");
+				this.litri = massimaDimensioneBottiglia;
+			}
+		}
+
+		public void Bevi(double litriDaBere)
+        {
+			if(this.litri - litriDaBere > 0)
             {
-				Console.WriteLine("Bottiglia di litri: " + litri + "Ammessa");
+				this.litri = this.litri - litriDaBere;
+				Console.WriteLine("glu glu glu, ho bevuto " + litriDaBere);
+			} else
+			{
+				Console.WriteLine("glu glu glu, hai bevuto tutta l'acqua disponibile");
+				this.litri = 0;
+			}
+		}
+
+		public void CalcoloDimensioneBottiglia()
+        {
+			if (litri <= massimaDimensioneBottiglia)
+            {
+				Console.WriteLine("Bottiglia di litri: " + litri + " Ammessa");
             } else
             {
 				Console.WriteLine("Impossibile vendere bottiglie d'acqua superiori al 1.5lt! ");
 			}
         }
 
+		public void StampaLitri()
+        {
+			Console.WriteLine("I litri della bottiglia sono: " + litri);
+		}
+
+		public static void ConvertiInGalloni(double litriDaConvertire)
+        {
+			decimal litroConvertito = Convert.ToDecimal(litriDaConvertire / gallone);
+			litroConvertito = Math.Truncate(litroConvertito * 1000m) / 1000m;
+			Console.WriteLine("--Conversione--");
+			Console.WriteLine(litriDaConvertire + "litri = " + litroConvertito + " gallone");
+		}
+
 		public override void StampaProdotto()
 		{
-			if (litri <= 1.5)
+			if (litri <= massimaDimensioneBottiglia)
 			{
 				base.StampaProdotto();
 				Console.WriteLine("Litri: " + litri);
+				Console.WriteLine("Ph: " + ph);
+				Console.WriteLine("Sorgente: " + sorgente);
 			} else
 			{
 				Console.WriteLine("Impossibile Stampare Prodotto");
--- CsharpShop2/Program.cs
+++ CsharpShop2/Program.cs
@@ -1,23 +1,27 @@
 ﻿using CsharpShop2;
 
 Prodotto Mioprodotto = new Prodotto("banane", "Provenienza Africa", 3, 4);
-Acqua bottiglia1 = new Acqua("Ferrarelle", "Minerale", 2.50, 4, 3);
+Acqua bottiglia1 = new Acqua("Ferrarelle", "Minerale", 2.50, 4, 1, 7.5, "Roccamonfina");
 
 int codice = Mioprodotto.GetCodice();
 Console.WriteLine("Il codice prodotto è: " + codice);
 
 
-Mioprodotto.StampaCalcoloPrezzoConIva();
-
-Mioprodotto.StampaPrezzoConIva();
-
-Mioprodotto.StampaPrezzoSenzaIva();
-
 Console.WriteLine("");
 
 Mioprodotto.StampaProdotto();
-
 Console.WriteLine("");
 
 bottiglia1.StampaProdotto();
+Console.WriteLine("");
+
+bottiglia1.Bevi(0.5);
+bottiglia1.StampaLitri();
+bottiglia1.Svuota();
+bottiglia1.Riempi(2);
+bottiglia1.Svuota();
+
+Console.WriteLine("");
+
+Acqua.ConvertiInGalloni(10);
 
