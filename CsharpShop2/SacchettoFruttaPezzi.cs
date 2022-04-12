e833b28ec5fd79a721f0bb4e83e0dd9377ac3c8f
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-12 17:21:52 +0200

    Update SacchettoFruttaPezzi.cs

--- CsharpShop2/SacchettoFruttaPezzi.cs
+++ CsharpShop2/SacchettoFruttaPezzi.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 namespace CsharpShop2
 {
-	public class SacchettoFruttaPezzi
+	public class SacchettoFruttaPezzi : Prodotto
 	{
-		public SacchettoFruttaPezzi()
+		private int pezzi;
+		public static readonly int massimiPezziSacchetto = 5;
+
+		public SacchettoFruttaPezzi(string nome, string descrizione, double prezzo, int iva, int pezzi) : base(nome, descrizione, prezzo, iva)
 		{
+			this.pezzi = pezzi;
+			CalcoloPezziSacchetto();
+		}
+
+		public int GetPezzi()
+		{
+			return pezzi;
+		}
+
+		public void CalcoloPezziSacchetto()
+		{
+			if (pezzi <= massimiPezziSacchetto)
+			{
+				Console.WriteLine("Pezzi nel sacchetto: " + pezzi + " Ammessa");
+			}
+			else
+			{
+				Console.WriteLine("Impossibile vendere sacchetto di frutta");
+			}
+		}
+
+		public override void StampaProdotto()
+		{
+			if (pezzi <= massimiPezziSacchetto)
+			{
+				base.StampaProdotto();
+				Console.WriteLine("Pezzi sacchetto: " + pezzi);
+				
+			}
+			else
+			{
+				Console.WriteLine("Impossibile Stampare Prodotto");
+			}
 		}
 	}
 }
