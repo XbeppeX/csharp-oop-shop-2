24e546f24718d8303c2a16def9e6f4ef430826a4
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-15 10:15:54 +0200

    Update CiboInScatola.cs

--- CsharpShop2/CiboInScatola.cs
+++ CsharpShop2/CiboInScatola.cs
@@ -3,11 +3,12 @@ namespace CsharpShop2
 {
 	public class CiboInScatola : Prodotto
 	{
+		private int peso;
 		public static readonly int massimoPesoScatola = 300;
 
-		public CiboInScatola(string nome, string descrizione, double prezzo, int iva) : base(nome, descrizione, prezzo, iva)
+		public CiboInScatola(string nome, string descrizione, double prezzo, int iva,int peso) : base(nome, descrizione, prezzo, iva)
 		{
-
+			this.peso = peso;
 		}
 
 		public int GetMassimoPesoScatola()
@@ -15,10 +16,30 @@ namespace CsharpShop2
 			return massimoPesoScatola;
 		}
 
+		public void CheckPeso()
+        {
+			if (peso <= massimoPesoScatola)
+			{
+				Console.WriteLine("Peso cibo in scatola: " + peso + " Ammessa");
+			}
+			else
+			{
+				Console.WriteLine("Impossibile vendere cibo in scatola superiore a: " + massimoPesoScatola + " g di peso");
+			}
+		}
+
 		public override void StampaProdotto()
 		{
-			base.StampaProdotto();
-			Console.WriteLine("Massimo peso per scatola : " + massimoPesoScatola);
+			if (peso <= massimoPesoScatola)
+			{
+				base.StampaProdotto();
+				Console.WriteLine("Massimo peso per scatola : " + massimoPesoScatola);
+			}
+			else
+			{
+				Console.WriteLine("Impossibile Stampare Prodotto");
+			}
+			
 		}
 	}
 }
